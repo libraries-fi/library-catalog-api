9b1d72ace87a8e943d891ebfcfafcbea91eed5db
Virgil Mocanu <mocanuvirgil@gmail.com>
2011-03-04 14:59:12 +0200

    Few layout changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 KirjastoData::Application.routes.draw do
-  resources :records, :only => [:index, :show]
+  resources :records, :only => :show
   match "/search/isbn" => "search#isbn", :as => :isbn_search, :via => :get
   match "/search/author" => "search#author", :as => :author_search, :via => :get
   match "/search/title" => "search#title", :as => :title_search, :via => :get
+  match "/search" => "pages#search", :as => :search_page, :via => :get
 
   root :to => "pages#home"
 end
